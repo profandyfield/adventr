2a2b996aaff3f3e7912cde7491f144782ba96009
Andy Field <andyf@sussex.ac.uk>
2017-09-07 12:56:03 +0100

    Updated Description file to u=include dependency on R 3.1.0

---
+++ R/data.R
@@ -0,0 +1,66 @@
+#' Subset of Ha et al. (2010) data
+#'
+#' A dataset containing a random sample of 20 female participants from Ha, T.,
+#' Overbeek, G., & Engels, R. C. M. E. (2010). Effects of Attractiveness and
+#' Social Status on Dating Desire in Heterosexual Adolescents: An Experimental
+#' Study. Archives of Sexual Behavior, 39(5), 1063-1071. doi: 10.1007/s10508-009-9561-z
+#'
+#'
+#' @format A data frame with 20 rows and 13 variables:
+#' \describe{
+#'   \item{ID}{Participant ID}
+#'   \item{ageYears}{Age of the participant, years}
+#'   \item{Sex}{Biological sex of the individual}
+#'   \item{HighSalary}{Rating (out of 10) of the importance of a high salary in a potential romantic partner}
+#'   \item{FinishedEducation}{Rating (out of 10) of the importance of a potential romantic partner having finished their education}
+#'   \item{Kind}{Rating (out of 10) of the importance of a potential romantic partner being kind}
+#'   \item{Humour}{Rating (out of 10) of the importance of a potential romantic partner having a sense of humour}
+#'   \item{Ambitious}{Rating (out of 10) of the importance of a potential romantic partner being ambitious}
+#'   \item{WantsChildren}{Rating (out of 10) of the importance of a potential romantic partner wanting children}
+#'   \item{Romantic}{Rating (out of 10) of the importance of a potential romantic partner being romantic}
+#'   \item{AttractiveAppearance}{Rating (out of 10) of the importance of a potential romantic partner being physically attractive}
+#'   \item{Creativity}{Rating (out of 10) of the importance of a potential romantic partner being creative}
+#'   \item{Honest}{Rating (out of 10) of the importance of a potential romantic partner being honest}
+#' }
+#' @source \url{https://www.discoveringstatistics.com/books/an-adventure-in-statistics/}
+
+"ais_ch03_ha"
+
+
+#' Alice's RAS scores
+#'
+#' A dataset containing Alice's ratings on the Relationship Assessment Scale (RAS)
+#' over 10 weeks
+#'
+#' @format A data frame with 10 rows and 2 variables:
+#' \describe{
+#'   \item{Week}{The week in which the rating was given}
+#'   \item{Rating}{Alice's score on the relaationship assessment scale}
+#' }
+#' @source \url{https://www.discoveringstatistics.com/books/an-adventure-in-statistics/}
+
+"ais_ch04_ras"
+
+
+
+#' JIG:SAW employee data.
+#'
+#' A dataset containing information about the characteristics of different groups of employees within
+#' the corporation called JIG:SAW and non-employees
+#'
+#' @format A data frame with 240 rows and 7 variables:
+#' \describe{
+#'   \item{ID}{employee ID}
+#'   \item{Employee}{whether or not the employee works for JIG:SAW}
+#'   \item{Job_Type}{Categories of employee}
+#'   \item{Footspeed}{Footspeed of the individual, miles per hour}
+#'   \item{Strength}{Maximal push force of the individual, Newtons}
+#'   \item{Vision}{Visual accuity, Newtons}
+#'   \item{Sex}{Biological sex of the individual}
+#' }
+#' @source \url{https://www.discoveringstatistics.com/books/an-adventure-in-statistics/}
+
+"ais_ch05_jigsaw"
+
+
+

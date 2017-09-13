f2fe73cbd457160107c0ff33ed43f93d1c33e5f4
profandyfield <andyfield@Professors-Mac-Pro.local>
2017-09-13 23:26:53 +0100

    change variable names tolower case

--- R/data.R
+++ R/data.R
@@ -8,23 +8,23 @@
 #'
 #' @format A data frame with 20 rows and 13 variables:
 #' \describe{
-#'   \item{ID}{Participant ID}
-#'   \item{ageYears}{Age of the participant, years}
+#'   \item{id}{Participant ID}
+#'   \item{age}{Age of the participant, years}
 #'   \item{Sex}{Biological sex of the individual}
-#'   \item{HighSalary}{Rating (out of 10) of the importance of a high salary in a potential romantic partner}
-#'   \item{FinishedEducation}{Rating (out of 10) of the importance of a potential romantic partner having finished their education}
-#'   \item{Kind}{Rating (out of 10) of the importance of a potential romantic partner being kind}
-#'   \item{Humour}{Rating (out of 10) of the importance of a potential romantic partner having a sense of humour}
-#'   \item{Ambitious}{Rating (out of 10) of the importance of a potential romantic partner being ambitious}
-#'   \item{WantsChildren}{Rating (out of 10) of the importance of a potential romantic partner wanting children}
-#'   \item{Romantic}{Rating (out of 10) of the importance of a potential romantic partner being romantic}
-#'   \item{AttractiveAppearance}{Rating (out of 10) of the importance of a potential romantic partner being physically attractive}
-#'   \item{Creativity}{Rating (out of 10) of the importance of a potential romantic partner being creative}
-#'   \item{Honest}{Rating (out of 10) of the importance of a potential romantic partner being honest}
+#'   \item{hi_salary}{Rating (out of 10) of the importance of a high salary in a potential romantic partner}
+#'   \item{fin_ed}{Rating (out of 10) of the importance of a potential romantic partner having finished their education}
+#'   \item{kind}{Rating (out of 10) of the importance of a potential romantic partner being kind}
+#'   \item{humour}{Rating (out of 10) of the importance of a potential romantic partner having a sense of humour}
+#'   \item{ambitious}{Rating (out of 10) of the importance of a potential romantic partner being ambitious}
+#'   \item{wants_child}{Rating (out of 10) of the importance of a potential romantic partner wanting children}
+#'   \item{romantic}{Rating (out of 10) of the importance of a potential romantic partner being romantic}
+#'   \item{attractive}{Rating (out of 10) of the importance of a potential romantic partner being physically attractive}
+#'   \item{creativity}{Rating (out of 10) of the importance of a potential romantic partner being creative}
+#'   \item{honest}{Rating (out of 10) of the importance of a potential romantic partner being honest}
 #' }
 #' @source \url{https://www.discoveringstatistics.com/books/an-adventure-in-statistics/}
 
-"ais_ch03_ha"
+"ha_dat"
 
 
 #' Alice's RAS scores
@@ -34,12 +34,12 @@
 #'
 #' @format A data frame with 10 rows and 2 variables:
 #' \describe{
-#'   \item{Week}{The week in which the rating was given}
-#'   \item{Rating}{Alice's score on the relaationship assessment scale}
+#'   \item{week}{The week in which the rating was given}
+#'   \item{rating}{Alice's score on the relaationship assessment scale}
 #' }
 #' @source \url{https://www.discoveringstatistics.com/books/an-adventure-in-statistics/}
 
-"ais_ch04_ras"
+"ras_dat"
 
 
 
@@ -50,17 +50,17 @@
 #'
 #' @format A data frame with 240 rows and 7 variables:
 #' \describe{
-#'   \item{ID}{employee ID}
-#'   \item{Employee}{whether or not the employee works for JIG:SAW}
-#'   \item{Job_Type}{Categories of employee}
-#'   \item{Footspeed}{Footspeed of the individual, miles per hour}
-#'   \item{Strength}{Maximal push force of the individual, Newtons}
-#'   \item{Vision}{Visual accuity, Newtons}
-#'   \item{Sex}{Biological sex of the individual}
+#'   \item{id}{employee ID}
+#'   \item{employee}{whether or not the employee works for JIG:SAW}
+#'   \item{job_type}{Categories of employee}
+#'   \item{footspeed}{Footspeed of the individual, miles per hour}
+#'   \item{strength}{Maximal push force of the individual, Newtons}
+#'   \item{vision}{Visual accuity}
+#'   \item{sex}{Biological sex of the individual}
 #' }
 #' @source \url{https://www.discoveringstatistics.com/books/an-adventure-in-statistics/}
 
-"ais_ch05_jigsaw"
+"jig_dat"
 
 
 

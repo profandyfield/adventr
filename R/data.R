880fbe2ddea6d70f1198935789c88e4b21dfe619
Andy Field <andyf@sussex.ac.uk>
2017-10-04 20:47:43 +0100

    New data file descriptions added

--- R/data.R
+++ R/data.R
@@ -122,3 +122,124 @@
 #' @source \url{https://www.discoveringstatistics.com/books/an-adventure-in-statistics/}
 
 "taser_dat"
+
+#' Invisibility paste data
+#'
+#' A dataset relating to the following fictitious experiment:
+#' "Twenty-eight participants were tested individually. Each participant sat on a revolving chair in the middle of a circle of 20 people.
+#' The 20 people all wore a light deflecting aid on their face. For half of the participants (N = 14) the 20 people wore a standard calcite-based mask,
+#' whereas the other half (N = 14) saw people wearing ‘invisibility paste’, a transparent cream containing calcite micro-crystals.
+#' Participants looked at each person and were given two photographs, one depicting the person they were looking at and one depicting a different
+#' person matched for age and sex. Participants had to decide which person they were looking at, before turning to the next person and being given
+#' two different photographs. Participants scored a point for each person they identified correctly: a score of 0 would mean they always chose the
+#' incorrect photo, a score of 20 would mean that they always chose the correct photo."
+#' The data are taken from Chapter 15 of Field, A. P. (2016). An adventure in statistics: the reality enigma. London: Sage.
+#'
+#' @format A tibble with 28 rows and 3 variables:
+#' \describe{
+#'   \item{id}{Participant ID}
+#'   \item{mask}{Whether the person was assigned to the calcite mask condition or the invisibility paste condition}
+#'   \item{recognition}{How many of the 20 people they correctly identified}
+#' }
+#' @source \url{https://www.discoveringstatistics.com/books/an-adventure-in-statistics/}
+
+"mask_dat"
+
+#' Calcite mask data
+#'
+#' A dataset relating to the following fictitious experiment, which is an extension of the invisibility paste data described elsewhere:
+#' "... we asked whether we could manipulate the face perceived by the participant when viewing someone wearing calcite invisibility paste.
+#' Participants were all so-called Chippers, who had ID chips installed in their brains. Each participant met 40 people who had calcite paste
+#' on their face. For each of these people we took a photo of their face (the ‘actual’ face), and found a photo of a different face matched for
+#' sex and age (the ‘different’ face). We could send these images to the participant’s visual cortex using their ID chip.
+#' For 20 of the encounters an image of the person’s ‘actual’ face was transmitted to the participant’s ID chip during the encounter, for the remaining
+#' 20 encounters the ‘different’ face was sent to the chip. During each encounter the participant was shown the photos of the actual and different face
+#' and pointed to the one depicting whom they thought they had just met. As in Experiment 1, participants scored a point for each person they correctly
+#' identified: a score of 0 would mean that they never chose the correct photo, a score of 20 would mean that they always chose the correct photo."
+#' The data are taken from Chapter 15 of Field, A. P. (2016). An adventure in statistics: the reality enigma. London: Sage.
+#'
+#' @format A tibble with 20 rows and 3 variables:
+#' \describe{
+#'   \item{id}{Participant ID}
+#'   \item{actual}{How many of the 20 people the participant correctly identified when a picture of the 'actual' person was transmitted to the participant's ID chip}
+#'   \item{different}{How many of the 20 people the participant correctly identified when a picture of a 'different' person was transmitted to the participant's ID chip}
+#' }
+#' @source \url{https://www.discoveringstatistics.com/books/an-adventure-in-statistics/}
+
+"calcite_dat"
+
+
+#' Memory erasing data
+#'
+#' A dataset relating to the following fictitious experiment:
+#' "Thirty-six participants with ID chips were tested individually. All participants met a stranger who had a 5-minute scripted conversation with
+#' them containing 10 critical pieces of information. A week later, the participants were asked to recall the encounter for 5 minutes, and then after
+#' a 10-minute break wrote everything that they could remember about the original encounter. During the 5-minute initial recall, one of three things
+#' happened: the control group (N = 12) received no intervention; the erase group (N = 12) had a pulse of electricity sent to their brain via their
+#' ID chip; and the replace group (N = 12) had conflicting verbal descriptions sent to their ID chip. The outcome was how many of the 10 critical
+#' pieces of information the participants wrote down after the recall phase."
+#' The data are taken from Chapter 16 of Field, A. P. (2016). An adventure in statistics: the reality enigma. London: Sage.
+#'
+#' @format A tibble with 36 rows and 7 variables:
+#' \describe{
+#'   \item{id}{Participant ID}
+#'   \item{group}{To which of the no intervention, memory erase or memory replace conditions the participant was assigned}
+#'   \item{recall}{How many of the 10 critical pieces of information the participant identified}
+#'   \item{erase_dummy}{Dummy variable that compares the erase condition to no intervention}
+#'   \item{replace_dummy}{Dummy variable that compares the replace condition to no intervention}
+#'   \item{contrast_1}{Variable that uses contrast codes to compare the memory conditions (combined) to no intervention}
+#'   \item{contrast_2}{Variable that uses contrast codes to compare the erase condition to the replace condition}
+#' }
+#' @source \url{https://www.discoveringstatistics.com/books/an-adventure-in-statistics/}
+
+"memory_dat"
+
+#' Memory implanting data
+#'
+#' A dataset relating to the following fictitious experiment:
+#' "We wanted to see whether our failure to replace memories was due to the type of conflicting information used. Five participants engaged in
+#' three different scripted encounters with strangers during which 10 unusual things happened. After each encounter participants were asked to
+#' privately recall what had happened. During the period of recall for one encounter they were left alone (control), during the recall of another
+#' their ID chip was used to implant a verbal description of the encounter in which the 10 unusual events were replaced with different events, and
+#' during the recall of the final encounter their ID chip was used to implant visual images of 10 different things to what they actually experienced.
+#' Each participant, therefore, experienced three encounters and a different type of recall manipulation followed each one; the type of manipulation
+#' associated with a particular encounter was counterbalanced. Finally, participants recalled the event for a second time and we measured how many
+#' of the 10 unusual events that they originally experienced that they could recall."
+#' The data are taken from Chapter 16 of Field, A. P. (2016). An adventure in statistics: the reality enigma. London: Sage.
+#'
+#' @format A tibble with 5 rows and 4 variables:
+#' \describe{
+#'   \item{id}{Participant ID}
+#'   \item{no_interference}{How many of the 10 unusual events were remembered when there was no interference}
+#'   \item{verbal}{How many of the 10 unusual events were remembered when verbal information was used as interference}
+#'   \item{visual}{How many of the 10 unusual events were remembered when visual images were used as interference}
+#' }
+#' @source \url{https://www.discoveringstatistics.com/books/an-adventure-in-statistics/}
+
+"implant_dat"
+
+#' Alice's gene data
+#'
+#' A dataset relating to the following fictitious experiment:
+#' Participants who had facial burns were recruited. One-week pre-test, a gene C–10XFMG (the so-called chameleon gene)
+#' was introduced into all participants.Two days prior to test, a genetic toggle switch was introduced into half of the participants.
+#' Half of the participants were asked to look at a photograph of themselves from before their injury and to imagine the cells in their faces
+#' changing to become like the photo. The remainder acted as a control and were asked to look at a picture of a same-sex stranger and to try
+#' to change their face to become the person in the picture. All participants looked at the picture for 6 sessions of 20 minutes each.
+#' At the end of the sessions their faces were scanned into a computer and compared to the face in the photograph.
+#' Facial recognition software produced a precise resemblance measure as a percentage (100% = the participants face is exactly like the face
+#' in the photograph, 0% the person in the photo bears no resemblance at all to the participant).
+#' The data are taken from Chapter 17 of Field, A. P. (2016). An adventure in statistics: the reality enigma. London: Sage.
+#'
+#' @format A tibble with 32 rows and 4 variables:
+#' \describe{
+#'   \item{id}{Participant ID}
+#'   \item{picture}{A factor describing whether a participant tried to resemble a picture of themselves (Self), or a matched stranger (Other)}
+#'   \item{gene}{A factor describing whether a participant receive dthe toggle switch (C-gene + Toggle) or not (C-gene)}
+#'   \item{resemblance}{How closely their face resembled the picture (100% = the participants face is exactly like the face
+#' in the photograph, 0% the person in the photo bears no resemblance at all to the participant)}
+#' }
+#' @source \url{https://www.discoveringstatistics.com/books/an-adventure-in-statistics/}
+
+"alice_dat"
+
